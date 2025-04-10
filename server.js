7e69de587a80d52db0978871bde40edf07ccd2a2
sravankingkumar <30718385+sravankingkumar@users.noreply.github.com>
2025-04-10 14:19:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
